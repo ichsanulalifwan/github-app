6ccef9e54a6e26704c196c65aa6bfd8803aae0b5
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2021-04-03 21:51:34 +0800

    setup ActionBar and menu

--- app/src/main/java/com/dicoding/picodiploma/githubapp/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/githubapp/ui/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.dicoding.picodiploma.githubapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.dicoding.picodiploma.githubapp.R
 import com.dicoding.picodiploma.githubapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +20,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val appBarConfiguration = AppBarConfiguration
+            .Builder(
+                R.id.userListFragment,
+                R.id.detailFragment
+            )
+            .build()
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.change_language) {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
