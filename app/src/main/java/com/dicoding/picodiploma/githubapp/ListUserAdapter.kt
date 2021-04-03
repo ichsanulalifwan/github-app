1110c0018b8d807472fe8416236f903fac40b3fd
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2021-04-03 14:00:42 +0800

    add model and adapter

---
+++ app/src/main/java/com/dicoding/picodiploma/githubapp/ListUserAdapter.kt
@@ -0,0 +1,54 @@
+package com.dicoding.picodiploma.githubapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.dicoding.picodiploma.githubapp.databinding.ItemUserBinding
+
+class ListUserAdapter: RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
+
+    private lateinit var onItemClickListener: OnItemClickListener
+    private val listUser = ArrayList<User>()
+
+    fun setData(list: ArrayList<User>) {
+        listUser.clear()
+        listUser.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.onItemClickListener = onItemClickListener
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListViewHolder {
+        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listUser[position])
+    }
+
+    override fun getItemCount(): Int = listUser.size
+
+    inner class ListViewHolder(private val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            with(binding){
+                Glide.with(itemView.context)
+                    .load(user.avatar)
+                    .apply(RequestOptions().override(55, 55))
+                    .into(imgAvatar)
+                txtUsername.text = user.username
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClicked(user: User)
+    }
+}
