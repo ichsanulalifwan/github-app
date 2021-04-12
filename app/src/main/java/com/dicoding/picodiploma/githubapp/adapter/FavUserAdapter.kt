63019fa15579fabe53531b97fa7113871e7ed0de
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2021-04-12 22:52:04 +0800

    add favorite user adapter and custom item_user layout

--- app/src/main/java/com/dicoding/picodiploma/githubapp/adapter/FavUserAdapter.kt
+++ app/src/main/java/com/dicoding/picodiploma/githubapp/adapter/FavUserAdapter.kt
@@ -5,46 +5,45 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.dicoding.picodiploma.githubapp.model.User
 import com.dicoding.picodiploma.githubapp.databinding.ItemUserBinding
+import com.dicoding.picodiploma.githubapp.db.entity.FavUser
 
-class ListUserAdapter : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
+class FavUserAdapter : RecyclerView.Adapter<FavUserAdapter.ListViewHolder>() {
 
     private lateinit var onItemClickListener: OnItemClickListener
-    private val listUser = ArrayList<User>()
+    private val listFavUser = ArrayList<FavUser>()
 
-    fun setData(list: ArrayList<User>) {
-        listUser.clear()
-        listUser.addAll(list)
+    fun setData(list: ArrayList<FavUser>) {
+        listFavUser.clear()
+        listFavUser.addAll(list)
         notifyDataSetChanged()
     }
 
-    fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
-        this.onItemClickListener = onItemClickListener
-    }
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ListViewHolder {
-        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ItemUserBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false)
         return ListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        holder.bind(listUser[position])
+        holder.bind(listFavUser[position])
     }
 
-    override fun getItemCount(): Int = listUser.size
+    override fun getItemCount(): Int = listFavUser.size
 
     inner class ListViewHolder(private val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(user: User) {
-            with(binding){
+        fun bind(user: FavUser) {
+            with(binding) {
                 Glide.with(itemView.context)
                     .load(user.avatar)
                     .apply(RequestOptions().override(55, 55))
                     .into(imgAvatar)
                 txtUsername.text = user.username
+                txtName.text = user.name
+                txtSite.text = user.location
+                txtCorporation.text = user.company
                 todoCard.setOnClickListener {
                     onItemClickListener.onItemClicked(user)
                 }
@@ -52,7 +51,11 @@ class ListUserAdapter : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
         }
     }
 
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.onItemClickListener = onItemClickListener
+    }
+
     interface OnItemClickListener {
-        fun onItemClicked(user: User)
+        fun onItemClicked(user: FavUser)
     }
 }
