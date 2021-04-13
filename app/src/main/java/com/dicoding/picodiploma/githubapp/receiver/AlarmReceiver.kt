3edce85fb391e2449b2af08838238e783aa6fba0
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2021-04-13 14:35:12 +0800

    Add AlarmReceiver

---
+++ app/src/main/java/com/dicoding/picodiploma/githubapp/receiver/AlarmReceiver.kt
@@ -0,0 +1,108 @@
+package com.dicoding.picodiploma.githubapp.receiver
+
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.dicoding.picodiploma.githubapp.R
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    companion object {
+        const val EXTRA_MESSAGE = "message"
+        const val ID_REMINDER = 100
+        private const val TIME_FORMAT = "HH:mm"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val message = intent?.getStringExtra(EXTRA_MESSAGE)
+        val notifId = ID_REMINDER
+        val title = "GitHub Reminder"
+
+        showToast(context, title, message)
+
+        message?.let {
+            showReminderNotification(context, title, message, notifId)
+        }
+    }
+
+    private fun showReminderNotification(context: Context?, title: String, message: String, notifId: Int) {
+
+        val channelId = "Channel_1"
+        val channelName = "GitHubApp Reminder"
+
+        val notificationManagerCompat = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+            .setSmallIcon(R.drawable.github_mark_32px)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setSound(alarmSound)
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+
+        // check for android Oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notifChannel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+            notifChannel.enableLights(true)
+            notifChannel.lightColor = Color.RED
+            notifChannel.enableVibration(true)
+            notifChannel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
+
+            notificationManagerCompat.createNotificationChannel(notifChannel)
+
+            builder.setChannelId(channelId)
+                /*.setSmallIcon(R.drawable.github_mark_32px)*/
+        }
+        notificationManagerCompat.notify(notifId, builder.build())
+    }
+
+    private fun showToast(context: Context?, title: String?, message: String?) {
+        Toast.makeText(context, "$title : $message", Toast.LENGTH_LONG).show()
+    }
+
+    fun setReminder(context: Context, time: String, message: String?) {
+
+        if (isDateInvalid(time, TIME_FORMAT)) return
+
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, AlarmReceiver::class.java)
+        intent.putExtra(EXTRA_MESSAGE, message)
+
+        val timeArray = time.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]))
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]))
+        calendar.set(Calendar.SECOND, 0)
+
+        val pendingIntent = PendingIntent.getBroadcast(context, ID_REMINDER, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
+
+        Toast.makeText(context, "Reminder alarm set up", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isDateInvalid(time: String, format: String): Boolean {
+        return try {
+            val tf = SimpleDateFormat(format, Locale.getDefault())
+            tf.isLenient = false
+            tf.parse(time)
+            false
+        } catch (e: ParseException) {
+            true
+        }
+    }
+}
