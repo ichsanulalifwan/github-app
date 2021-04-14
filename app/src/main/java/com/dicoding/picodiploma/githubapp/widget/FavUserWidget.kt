2ba0f75ce3d69f53e1eb3ff9e08650c31318ab86
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2021-04-14 12:25:54 +0800

    init widget and add item layout

--- app/src/main/java/com/dicoding/picodiploma/githubapp/widget/FavUserWidget.kt
+++ app/src/main/java/com/dicoding/picodiploma/githubapp/widget/FavUserWidget.kt
@@ -1,14 +1,68 @@
 package com.dicoding.picodiploma.githubapp.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
+import android.widget.Toast
+import androidx.core.net.toUri
+import com.dicoding.picodiploma.githubapp.R
 
 /**
  * Implementation of App Widget functionality.
  */
 class FavUserWidget : AppWidgetProvider() {
+
+    companion object {
+
+        private const val TOAST_ACTION = "com.dicoding.picodiploma.TOAST_ACTION"
+        const val EXTRA_ITEM = "com.dicoding.picodiploma.EXTRA_ITEM"
+
+        private fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            val intent = Intent(context, StackWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+
+            val widgetText = context.getString(R.string.appwidget_text)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.fav_user_widget)
+            views.setTextViewText(R.id.appwidget_text, widgetText)
+            views.setRemoteAdapter(R.id.stack_view, intent)
+            views.setEmptyView(R.id.stack_view, R.id.empty_view)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+
+            val toastIntent = Intent(context, FavUserWidget::class.java)
+            toastIntent.action = TOAST_ACTION
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+            val toastPendingIntent = PendingIntent.getBroadcast(
+                context,
+                0,
+                toastIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if (intent.action != null) {
+            if (intent.action == TOAST_ACTION) {
+                val viewIndex = intent.getIntExtra(EXTRA_ITEM, 0)
+                Toast.makeText(context, "Touched view $viewIndex", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -27,18 +81,4 @@ class FavUserWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
-
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.fav_user_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
 }
