1824c7e8417fed9ed0ff27e6fa0394f04e37ee73
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2021-04-14 14:46:28 +0800

    add spalash screen

---
+++ app/src/main/java/com/dicoding/picodiploma/githubapp/ui/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.dicoding.picodiploma.githubapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.dicoding.picodiploma.githubapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
